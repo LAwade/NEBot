0f8eaf999142179646733cf2f1ee028fb7521ec1
Lucas Awade <lucasawade46@gmail.com>
2022-04-03 18:50:13 -0400

    test

--- app/Console/command.php
+++ app/Console/command.php
@@ -5,6 +5,7 @@ require_once __DIR__ . "/../../config/includes.php";
 $idbot = $argv[1] ?? $CONF_BOT_SERVER_ID;
 
 if ($idbot) {
+    echo $idbot;
     $command = new App\Consumers\CommandBot();
     $command->start($idbot);
 }
