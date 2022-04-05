7a495902d9717632b917ae943c527a0f47fe8784
Lucas Awade <lucasawade46@gmail.com>
2022-04-05 13:09:52 -0400

    test

--- app/Console/command.php
+++ app/Console/command.php
@@ -1,9 +1,13 @@
 <?php
 
+var_dump("VARIAVEL AMBIENTE: " . getenv('CONF_BOT_SERVER_ID'));
+
 require_once __DIR__ . "/../../config/includes.php";
 
 use React\EventLoop\Loop;
 use App\Consumers\CommandBot;
+
+var_dump(getenv('CONF_BOT_SERVER_ID'));
 
 $idbot = $argv[1] ?? getenv('CONF_BOT_SERVER_ID');
 
