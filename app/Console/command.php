99ab66d4c360435d6c8122df1d1f45ac6a28472a
Lucas Awade <lucasawade46@gmail.com>
2022-04-06 12:45:04 -0400

    test

--- app/Console/command.php
+++ app/Console/command.php
@@ -2,15 +2,11 @@
 
 require_once __DIR__ . "/../../config/includes.php";
 
-use React\EventLoop\Loop;
 use App\Consumers\CommandBot;
 
-var_dump(getenv('CONF_BOT_SERVER_ID'));
+$idbot = getenv('CONF_BOT_SERVER_ID');
 
-$idbot = $argv[1] ?? getenv('CONF_BOT_SERVER_ID');
-
-$timer = Loop::addPeriodicTimer(10, function () use ($idbot) {
-    echo $idbot;
+if($idbot)
     $command = new CommandBot();
     $command->start($idbot);
-});
+}
