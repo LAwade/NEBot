36671d3582738c4f1f2511f32c3549a0a1075649
Lucas Awade <lucasawade46@gmail.com>
2022-11-13 14:04:23 -0400

    teste

--- app/Cases/TibiaBOTCase.php
+++ app/Cases/TibiaBOTCase.php
@@ -367,7 +367,7 @@ class TibiaBOTCase
 
         logger('deaths')->debug("Notify: ". print_r($notify, true));
         logger('deaths')->debug("Player: ". print_r($player, true));
-        logger('deaths')->debug("lastDeath: ". print_r($lastDeath, true));
+        logger('deaths')->debug("Death: ". print_r($death, true));
 
         if ($lastDeath[0]) {
             $notify->deaths = date('Y-m-d H:i:s', strtotime($lastDeath[0]));
