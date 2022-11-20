7612253cd30efa945948079c0ce7a79523dc73ed
Lucas Awade <lucasawade46@gmail.com>
2022-11-20 18:30:23 -0400

    teste

--- app/Console/consumer.php
+++ app/Console/consumer.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("memory_limit", "512M");
-
+ob_flush();
 require_once __DIR__ . "/../../config/includes.php";
 
 use App\Cases\TibiaBOTCase;
@@ -13,6 +13,6 @@ use React\EventLoop\Loop;
 
 $bot = new TibiaBOTCase(new TibiaProvider, new TSAdminProvider, new MongoDBProvider, new MessageTSProvider);
 
-$timer = Loop::addPeriodicTimer(10, function () use ($bot) {
+$timer = Loop::addPeriodicTimer(20, function () use ($bot) {
     $bot->handle();
 });
